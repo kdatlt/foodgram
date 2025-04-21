5245691b92ec61916f57477fddf2e3e9de0d9524
Dmitry Korobeinikov <kda-tlt@yandex.ru>
2025-04-21 08:55:56 +0400

    added the structure of apps

---
+++ backend/api/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Позволяет изменять и удалять контент только его авторам.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+        )
