efa4b442586e51ccc462bcadd552a8977ec36802
Dmitry Korobeinikov <kda-tlt@yandex.ru>
2025-05-07 20:48:03 +0400

    Победил фильт

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filter',
+    'django_filters',
     'rest_framework.authtoken',
     'djoser',
 ]
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import GroupAdmin, UserAdmin
-from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
 
 from .models import Subscription, User
 
