1aa2f0652e6fbda98429fc8070d825677a7932ba
Dmitry Korobeinikov <kda-tlt@yandex.ru>
2025-05-12 16:18:55 +0400

    MyUser_User2

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,11 +1,11 @@
-from django.contrib.admin import register
+from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import MyUser
+from .models import User
 
 
-@register(MyUser)
-class MyUserAdmin(UserAdmin):
+@admin.register(User)
+class UserAdmin(UserAdmin):
     list_display = ('pk', 'username', 'email', 'first_name', 'last_name',
                     'password')
     list_filter = ('username', 'email')
