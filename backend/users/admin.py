8b1f36410a1d51275a4277aee0077ac962ccab6e
Dmitry Korobeinikov <kda-tlt@yandex.ru>
2025-05-16 11:16:18 +0400

    display2

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,12 +1,18 @@
-from django.contrib.admin import register
-from django.contrib.auth.admin import UserAdmin as Admin
+from django.contrib import admin
+from django.contrib.admin import display
 
 from .models import User
 
 
-@register(User)
-class UserAdmin(Admin):
+class UserAdmin(admin.ModelAdmin):
     list_display = ('pk', 'username', 'email', 'first_name', 'last_name',
                     'password', 'get_number_of_recipes')
     list_filter = ('username', 'email')
     search_fields = ('email', 'username')
+
+    @display(description='Количество рецептов')
+    def get_number_of_recipes(self, obj):
+        return obj.get_number_of_recipes()
+
+
+admin.site.register(User, UserAdmin)
